930eaf690202e025be4f9ed7bcd6e09bcb27cafe
justicepelteir <justice.douglas@hey.com>
2023-01-18 16:11:00 -0800

    add dummy text

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem(){
-    return <h2>Expense item!</h2>
+    return(
+        <div>
+            <div>February, 2, 2023</div>
+            <div>
+                <h2>Pet Insurance</h2>
+                <div>$200.00</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
