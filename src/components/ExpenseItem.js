33fc9fd45e7515627f595ea658867af77a7b1711
justicepelteir <justice.douglas@hey.com>
2023-01-18 16:43:57 -0800

    add jsx variable data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(){
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Pet Insurance';
+    const expenseAmount = 200.00;
+
     return(
         <div className='expense-item'>
-            <div>February, 2, 2023</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>Pet Insurance</h2>
-                <div className='expense-item__price'>$200.00</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>{expenseAmount}</div>
             </div>
         </div>
     );
